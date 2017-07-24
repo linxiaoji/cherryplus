6e1ffafdcd9ba31f182f4a7aca66e40d214e55e5
ksrt12 <kazakov12stepan2012@live.ru>
2017-07-24 14:54:39 +0300

    RIL + patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ export RR_BUILDTYPE="Nightly";
 #export WITH_ROOT_METHOD="magisk";
 export WITH_ROOT_METHOD="rootless";
 if [ -z $dtl ];then export dtl=0; else export days_to_log=$dtl; fi
+#export dtl=0; unset days_to_log;
 #export USE_NINJA=false
 if [ ! -e device/huawei/cherryplus/patches/apply ]
 then sh device/huawei/cherryplus/patches/apply.sh; touch device/huawei/cherryplus/patches/apply; fi
